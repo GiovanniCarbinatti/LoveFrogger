986178b94ff9d54f53f685a361c5a15f316b7974
Giovanni Carbinatti <83474533+GiovanniCarbinatti@users.noreply.github.com>
2024-05-26 22:19:35 -0300

    add 1 pixel to river height draw to a smoother transition between it and safezone

--- classes/River.lua
+++ classes/River.lua
@@ -7,7 +7,7 @@ function River(x, y, w, h)
 
         render = function(self)
             love.graphics.setColor(0,0,1)
-            love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)
+            love.graphics.rectangle("fill", self.x, self.y, self.w, self.h + 1)
         end
     }
 end
