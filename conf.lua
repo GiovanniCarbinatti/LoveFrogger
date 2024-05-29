fb3571f0ca4b3ec2335bbdd5efa49be08a59b212
Giovanni Carbinatti <83474533+GiovanniCarbinatti@users.noreply.github.com>
2024-05-28 21:18:23 -0300

    disables unused modules

--- conf.lua
+++ conf.lua
@@ -2,4 +2,8 @@ local love = require("love")
 
 function love.conf(t)
     t.window.title = "Moon Frogger"
+    
+    t.modules.joystick = false
+    t.modules.physics = false
+    t.modules.touch = false
 end
