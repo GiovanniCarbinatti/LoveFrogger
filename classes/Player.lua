0d314a060959bd40483ac44b0f3bd6c5f1ac23fa
Giovanni Carbinatti <83474533+GiovanniCarbinatti@users.noreply.github.com>
2024-05-29 12:15:45 -0300

    add frog rotation to face current direction

--- classes/Player.lua
+++ classes/Player.lua
@@ -1,6 +1,11 @@
 local love = require("love")
 local jump = love.audio.newSource("assets/jump.mp3", "static")
-local frog_sprite = love.graphics.newImage('assets/frog.png')
+local frog_sprite = {
+    love.graphics.newImage('assets/frog.png'),
+    love.graphics.newImage('assets/frogL.png'),
+    love.graphics.newImage('assets/frogR.png'),
+    love.graphics.newImage('assets/frogD.png')
+}
 
 function Player() 
     return {
@@ -8,6 +13,7 @@ function Player()
     y = VIRTUAL_HEIGHT - 32,
     w = 16,
     h = 16,
+    sprite = 1,
     move_delay = 0,
 
     move = function(self)
@@ -15,30 +21,43 @@ function Player()
 
         function love.keypressed(key)
             if     key == "w" or key == "up"    and self.move_delay < 0 then 
-                self.y = self.y - 16 self.move_delay = 12 jump:play() state.score = state.score + 10
+                self.sprite = 1 
+                self.y = self.y - 16 
+                self.move_delay = 12 
+                jump:play() 
+                state.score = state.score + 10
             elseif key == "s" or key == "down"  and self.move_delay < 0 then
-                if self.y + self.h + 16 < VIRTUAL_HEIGHT then self.y = self.y + 16 self.move_delay = 12 jump:play() end
+                self.sprite = 4
+                if self.y + self.h + 16 < VIRTUAL_HEIGHT then 
+                    self.y = self.y + 16 
+                    self.move_delay = 12 
+                    jump:play() 
+                end
             elseif key == "a" or key == "left"  and self.move_delay < 0 then
                 if self.x - 16 > 0 then 
                     self.x = self.x - 16
                 else
                     self.x = 0
                 end
-                self.move_delay = 12 jump:play()
+                self.sprite = 2 
+                self.move_delay = 12 
+                jump:play()
             elseif key == "d" or key == "right" and self.move_delay < 0 then
                 if self.x + self.w + 16 < VIRTUAL_WIDTH then
                     self.x = self.x + 16
                 else
                     self.x = VIRTUAL_WIDTH - 16
                 end
-                self.move_delay = 12 jump:play()
+                self.sprite = 3 
+                self.move_delay = 12 
+                jump:play()
             end
         end
     end,
 
     render = function(self)
         love.graphics.setColor(1,1,1)
-        love.graphics.draw(frog_sprite, self.x, self.y)
+        love.graphics.draw(frog_sprite[self.sprite], self.x, self.y)
     end
     }
 end
