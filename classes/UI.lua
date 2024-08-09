4479d291314d2c93f5db4ca49a7295fead2d0445
Giovanni Carbinatti <83474533+GiovanniCarbinatti@users.noreply.github.com>
2024-08-09 12:47:45 -0300

    add a better UI to the bottom screen

--- classes/UI.lua
+++ classes/UI.lua
@@ -1,13 +1,16 @@
 local retro_font = love.graphics.newFont('assets/font.ttf', 16)
 local safesprite = love.graphics.newImage('assets/safezone.png')
 local lifesprite = love.graphics.newImage('assets/life.png')
+local bottom_ui  = love.graphics.newImage('assets/ui_bottom.png')
+
 
 local UI = {}
 
 function UI.render()
     love.graphics.setColor(1,0,0)
     love.graphics.rectangle("fill", 0, 0, VIRTUAL_WIDTH, 16)
-    love.graphics.rectangle("fill", 0, VIRTUAL_HEIGHT-16, VIRTUAL_WIDTH, 16)
+    love.graphics.setColor(1,1,1)
+    love.graphics.draw(bottom_ui, 0, VIRTUAL_HEIGHT-16)
 
     love.graphics.setFont(retro_font)
     love.graphics.setColor(0,0,0)
@@ -17,7 +20,7 @@ function UI.render()
     love.graphics.setColor(0,0,1)
     for i = 1, state.lifes do
         love.graphics.setColor(1,1,1)
-        love.graphics.draw(lifesprite, (i - 1) * 12 + 4, VIRTUAL_HEIGHT - 12)
+        love.graphics.draw(lifesprite, (i - 1) * 12 + 6, VIRTUAL_HEIGHT - 12)
     end
 end
 
